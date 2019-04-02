9b43324d92d0ef6061aedf9ec9b724400cbe4047
Satyam Ghodasara <sghodas@gmail.com>
2019-04-02 16:44:30 -0400

    Moved line drawing to WordBoxImageView

--- LunarReader/View Controllers/ViewController.swift
+++ LunarReader/View Controllers/ViewController.swift
@@ -8,11 +8,10 @@
 
 import UIKit
 import Vision
-import AVFoundation
 
 class ViewController: UIViewController, WordBoxFinderDelegate, UIScrollViewDelegate {
     
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var wordBoxImageView: WordBoxImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,59 +22,20 @@ class ViewController: UIViewController, WordBoxFinderDelegate, UIScrollViewDeleg
         super.viewDidAppear(animated)
         // Do any additional setup after the view appears.
         
-        let wordBoxFinder = WordBoxFinder(image: self.imageView.image!, delegate: self)
+        let wordBoxFinder = WordBoxFinder(image: self.wordBoxImageView.image!, delegate: self)
         try? wordBoxFinder.findWordBoxes()
     }
     
     // MARK: - WordBoxFinder delegate
     
     func didFindWordBoxes(wordBoxFinder: WordBoxFinder, wordBoxes: [CGRect]) {
-        // Calculate image frame and offset within image view
-        let imageFrame = AVMakeRect(aspectRatio: self.imageView.image!.size, insideRect: self.imageView.bounds)
-        let yOffset = 0.5 * (self.imageView.frame.size.height - imageFrame.size.height)
-        
-        // Draw each word box
-        wordBoxes.forEach { wordBox in
-            let x = wordBox.minX * imageFrame.size.width
-            let y = wordBox.minY * imageFrame.size.height + yOffset
-            let width = wordBox.width * imageFrame.size.width
-            let height = wordBox.height * imageFrame.size.height
-            let scaledWordBox = CGRect(x: x, y: y, width: width, height: height)
-            let topOffset = scaledWordBox.height * 0.6
-            let bottomOffset = scaledWordBox.height * 0.4
-            
-            let topLineStart = CGPoint(x: scaledWordBox.minX, y: scaledWordBox.maxY - topOffset)
-            let topLineEnd = CGPoint(x: scaledWordBox.maxX, y: scaledWordBox.maxY - topOffset)
-            let bottomLineStart = CGPoint(x: scaledWordBox.minX, y: scaledWordBox.maxY - bottomOffset)
-            let bottomLineEnd = CGPoint(x: scaledWordBox.maxX, y: scaledWordBox.maxY - bottomOffset)
-
-            let topLinePath = UIBezierPath()
-            topLinePath.move(to: topLineStart)
-            topLinePath.addLine(to: topLineEnd)
-            let bottomLinePath = UIBezierPath()
-            bottomLinePath.move(to: bottomLineStart)
-            bottomLinePath.addLine(to: bottomLineEnd)
-            let topLine = CAShapeLayer()
-            topLine.path = topLinePath.cgPath
-            topLine.fillColor = nil
-            topLine.strokeColor = UIColor.black.cgColor
-            topLine.lineWidth = 0.7
-            topLine.lineCap = .round
-            let bottomLine = CAShapeLayer()
-            bottomLine.path = bottomLinePath.cgPath
-            bottomLine.fillColor = nil
-            bottomLine.strokeColor = UIColor.black.cgColor
-            bottomLine.lineWidth = 0.7
-            bottomLine.lineCap = .round
-            self.imageView.layer.addSublayer(topLine)
-            self.imageView.layer.addSublayer(bottomLine)
-        }
+        self.wordBoxImageView.drawLines(wordBoxes: wordBoxes)
     }
     
     // MARK: - UIScrollView delegate
     
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        return self.imageView
+        return self.wordBoxImageView
     }
 
 }
---
+++ LunarReader/Views/WordBoxImageView.swift
@@ -0,0 +1,58 @@
+//
+//  WordBoxImageView.swift
+//  LunarReader
+//
+//  Created by Satyam Ghodasara on 4/2/19.
+//  Copyright © 2019 Satyam Ghodasara. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import AVFoundation
+
+class WordBoxImageView: UIImageView {
+    
+    func drawLines(wordBoxes: [CGRect]) {
+        // Calculate image frame and offset within image view
+        let imageFrame = AVMakeRect(aspectRatio: self.image!.size, insideRect: self.bounds)
+        let yOffset = 0.5 * (self.frame.size.height - imageFrame.size.height)
+        
+        // Draw each word box's lines
+        wordBoxes.forEach { wordBox in
+            let x = wordBox.minX * imageFrame.size.width
+            let y = wordBox.minY * imageFrame.size.height + yOffset
+            let width = wordBox.width * imageFrame.size.width
+            let height = wordBox.height * imageFrame.size.height
+            let scaledWordBox = CGRect(x: x, y: y, width: width, height: height)
+            let topOffset = scaledWordBox.height * 0.6
+            let bottomOffset = scaledWordBox.height * 0.4
+            
+            let topLineStart = CGPoint(x: scaledWordBox.minX, y: scaledWordBox.maxY - topOffset)
+            let topLineEnd = CGPoint(x: scaledWordBox.maxX, y: scaledWordBox.maxY - topOffset)
+            let bottomLineStart = CGPoint(x: scaledWordBox.minX, y: scaledWordBox.maxY - bottomOffset)
+            let bottomLineEnd = CGPoint(x: scaledWordBox.maxX, y: scaledWordBox.maxY - bottomOffset)
+            
+            let topLinePath = UIBezierPath()
+            topLinePath.move(to: topLineStart)
+            topLinePath.addLine(to: topLineEnd)
+            let bottomLinePath = UIBezierPath()
+            bottomLinePath.move(to: bottomLineStart)
+            bottomLinePath.addLine(to: bottomLineEnd)
+            let topLine = CAShapeLayer()
+            topLine.path = topLinePath.cgPath
+            topLine.fillColor = nil
+            topLine.strokeColor = UIColor.black.cgColor
+            topLine.lineWidth = 0.7
+            topLine.lineCap = .round
+            let bottomLine = CAShapeLayer()
+            bottomLine.path = bottomLinePath.cgPath
+            bottomLine.fillColor = nil
+            bottomLine.strokeColor = UIColor.black.cgColor
+            bottomLine.lineWidth = 0.7
+            bottomLine.lineCap = .round
+            self.layer.addSublayer(topLine)
+            self.layer.addSublayer(bottomLine)
+        }
+    }
+    
+}
