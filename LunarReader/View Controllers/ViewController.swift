d8fbf40113fcdadb34da403347a0a2c134a60330
Satyam Ghodasara <sghodas@gmail.com>
2019-04-02 15:38:44 -0400

    Update word box drawing to line drawing

--- LunarReader/View Controllers/ViewController.swift
+++ LunarReader/View Controllers/ViewController.swift
@@ -41,12 +41,35 @@ class ViewController: UIViewController, WordBoxFinderDelegate {
             let y = wordBox.minY * imageFrame.size.height + yOffset
             let width = wordBox.width * imageFrame.size.width
             let height = wordBox.height * imageFrame.size.height
+            let scaledWordBox = CGRect(x: x, y: y, width: width, height: height)
+            let topOffset = scaledWordBox.height * 0.6
+            let bottomOffset = scaledWordBox.height * 0.4
             
-            let box = CALayer()
-            box.frame = CGRect(x: x, y: y, width: width, height: height)
-            box.borderWidth = 2.0
-            box.borderColor = UIColor.red.cgColor
-            self.imageView.layer.addSublayer(box)
+            let topLineStart = CGPoint(x: scaledWordBox.minX, y: scaledWordBox.maxY - topOffset)
+            let topLineEnd = CGPoint(x: scaledWordBox.maxX, y: scaledWordBox.maxY - topOffset)
+            let bottomLineStart = CGPoint(x: scaledWordBox.minX, y: scaledWordBox.maxY - bottomOffset)
+            let bottomLineEnd = CGPoint(x: scaledWordBox.maxX, y: scaledWordBox.maxY - bottomOffset)
+
+            let topLinePath = UIBezierPath()
+            topLinePath.move(to: topLineStart)
+            topLinePath.addLine(to: topLineEnd)
+            let bottomLinePath = UIBezierPath()
+            bottomLinePath.move(to: bottomLineStart)
+            bottomLinePath.addLine(to: bottomLineEnd)
+            let topLine = CAShapeLayer()
+            topLine.path = topLinePath.cgPath
+            topLine.fillColor = nil
+            topLine.strokeColor = UIColor.black.cgColor
+            topLine.lineWidth = 0.7
+            topLine.lineCap = .round
+            let bottomLine = CAShapeLayer()
+            bottomLine.path = bottomLinePath.cgPath
+            bottomLine.fillColor = nil
+            bottomLine.strokeColor = UIColor.black.cgColor
+            bottomLine.lineWidth = 0.7
+            bottomLine.lineCap = .round
+            self.imageView.layer.addSublayer(topLine)
+            self.imageView.layer.addSublayer(bottomLine)
         }
     }
 
