508c4049f3a4b650f52d925481decbafd00d45a0
Satyam Ghodasara <sghodas@gmail.com>
2019-04-06 18:30:00 -0400

    Add disabling of “Done” button when there’s no text

--- LunarReader/View Controllers/RenamePageTableViewController.swift
+++ LunarReader/View Controllers/RenamePageTableViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class RenamePageTableViewController: UITableViewController {
+class RenamePageTableViewController: UITableViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var doneButton: UIBarButtonItem!
     @IBOutlet weak var pageNameField: UITextField!
     
     private var newPageTableViewController: NewPageTableViewController?
@@ -26,10 +27,33 @@ class RenamePageTableViewController: UITableViewController {
     
     // MARK: - Button Actions
     
-    @IBAction func didTapDoneButton(_ sender: UIBarButtonItem) {
+    @IBAction func didTapDoneButton(_ sender: Any) {
         // Set the page name on the NewPageViewController and go back to it
         self.newPageTableViewController?.pageNameLabel.text = self.pageNameField.text
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.pageNameField.resignFirstResponder()
+        self.didTapDoneButton(textField)
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let text = (textField.text as NSString?)?.replacingCharacters(in: range, with: string)
+        if let emptyText = text?.isEmpty, emptyText {
+            self.doneButton.isEnabled = false
+        } else {
+            self.doneButton.isEnabled = true
+        }
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        self.doneButton.isEnabled = false
+        return true
     }
 
 }
