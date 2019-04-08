ce63c811c5f1d541e23dae71b683550b1d5a93a3
Satyam Ghodasara <sghodas@gmail.com>
2019-04-08 14:19:04 -0400

    Add custom TextFieldTableViewCell

---
+++ LunarReader/Views/TextFieldTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TextFieldTableViewCell.swift
+//  LunarReader
+//
+//  Created by Satyam Ghodasara on 4/8/19.
+//  Copyright © 2019 Satyam Ghodasara. All rights reserved.
+//
+
+import UIKit
+
+class TextFieldTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var textField: UITextField!
+    
+}
