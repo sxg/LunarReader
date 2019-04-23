66fb58a10cd21bdd952bd9e77d5b6d55bc72d7ea
Satyam Ghodasara <sghodas@gmail.com>
2019-04-23 15:16:41 -0400

    Add ReaderSegue preparation

--- LunarReader/View Controllers/DrawerDetailViewController.swift
+++ LunarReader/View Controllers/DrawerDetailViewController.swift
@@ -124,12 +124,12 @@ class DrawerDetailViewController: UIViewController, PulleyDrawerViewControllerDe
     
     // MARK: - Navigation
     
-    /*
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ReaderSegue" {
+            let readerViewController = (segue.destination as! UINavigationController).topViewController as! ReaderViewController
+            let page = sender as! Page
+            readerViewController.page = page
+        }
+    }
 
 }
