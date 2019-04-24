6df05ff6cbce59e98d1c0d1c8de92ba62c310c58
Satyam Ghodasara <sghodas@gmail.com>
2019-04-24 16:51:08 -0400

    Implement display/hiding of line width stepper

--- LunarReader/View Controllers/ReaderViewController.swift
+++ LunarReader/View Controllers/ReaderViewController.swift
@@ -12,6 +12,8 @@ class ReaderViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     
+    private weak var controlBarView: UIView?
+    
     public var page: Page?
 
     override func viewDidLoad() {
@@ -23,6 +25,43 @@ class ReaderViewController: UIViewController {
         // Set the page
         self.imageView.image = self.page!.image
     }
+    
+    // MARK: - Button Actions
+    
+    @IBAction func didTapLineWidthButton(_ sender: UIBarButtonItem) {
+        switch self.controlBarView {
+        case is LineWidthView:
+            // Dismiss the line width stepper
+            UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseInOut, animations: {
+                self.controlBarView!.frame = self.controlBarView!.frame.offsetBy(dx: 0, dy: self.controlBarView!.frame.height)
+                self.controlBarView!.alpha = 0
+            }) { finished in
+                // Unset the control bar
+                self.controlBarView!.removeFromSuperview()
+                self.controlBarView = nil
+            }
+        case is UIView:
+            // Replace the control bar
+            let lineWidthView = UINib(nibName: "LineWidthView", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! LineWidthView
+            lineWidthView.frame = self.controlBarView!.frame
+            self.view.addSubview(lineWidthView)
+            self.controlBarView!.removeFromSuperview()
+            // Set the new control bar
+            self.controlBarView = lineWidthView
+        default:
+            // Slide the line width stepper up if there is no control bar
+            let lineWidthView = UINib(nibName: "LineWidthView", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! LineWidthView
+            lineWidthView.frame = CGRect(x: self.view.frame.minX, y: self.view.frame.height, width: self.view.frame.width, height: 56)
+            lineWidthView.alpha = 0
+            self.view.addSubview(lineWidthView)
+            UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseInOut, animations: {
+                lineWidthView.frame = lineWidthView.frame.offsetBy(dx: 0, dy: -lineWidthView.frame.height)
+                lineWidthView.alpha = 1
+            })
+            // Set the new control bar
+            self.controlBarView = lineWidthView
+        }
+    }
 
     // MARK: - Navigation
     
@@ -30,4 +69,10 @@ class ReaderViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
 
+}
+
+private enum ControlBar {
+    case rotationSlider
+    case lineWidthStepper
+    case colorPicker
 }
