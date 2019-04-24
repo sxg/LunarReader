d31bfb02061222ee175ca1fd7d71c33a7e902263
Satyam Ghodasara <sghodas@gmail.com>
2019-04-24 14:56:39 -0400

    Add LineWidthView

---
+++ LunarReader/Views/LineWidthView.swift
@@ -0,0 +1,16 @@
+//
+//  LineWidthView.swift
+//  LunarReader
+//
+//  Created by Satyam Ghodasara on 4/24/19.
+//  Copyright © 2019 Satyam Ghodasara. All rights reserved.
+//
+
+import UIKit
+import ValueStepper
+
+class LineWidthView: UIView {
+
+    @IBOutlet weak var stepper: ValueStepper!
+
+}
