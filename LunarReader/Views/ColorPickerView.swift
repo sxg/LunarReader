99460b8e6e0ba875abac405fbeb4e4f9695d64d5
Satyam Ghodasara <sghodas@gmail.com>
2019-04-24 17:35:31 -0400

    Add ColorPickerView

---
+++ LunarReader/Views/ColorPickerView.swift
@@ -0,0 +1,23 @@
+//
+//  ColorPickerView.swift
+//  LunarReader
+//
+//  Created by Satyam Ghodasara on 4/24/19.
+//  Copyright © 2019 Satyam Ghodasara. All rights reserved.
+//
+
+import UIKit
+
+class ColorPickerView: UIView {
+
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var orangeButton: UIButton!
+    @IBOutlet weak var yellowButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    @IBOutlet weak var lightBlueButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var whiteButton: UIButton!
+    @IBOutlet weak var grayButton: UIButton!
+    @IBOutlet weak var blackButton: UIButton!
+    
+}
